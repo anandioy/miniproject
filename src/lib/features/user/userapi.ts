4dece87d9e80c00fb78d26ccc1a590a47b7d4d59
anandioy <anandioy@gmail.com>
2024-05-11 01:30:44 +0700

    cobacoba event

---
+++ src/lib/features/user/userapi.ts
@@ -0,0 +1,13 @@
+// import instance from "../../../utils/axiosInstance";
+
+// export const getUserProfile = async (userId) => {
+
+//     try {
+//         const response = await instance.get(`/users/${userId}`);
+
+//         return response.data;
+//     } catch (error) {
+//         throw new Error('Failed to fetch user data');
+//     }
+
+// };
